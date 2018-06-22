46d328a2f847dc2502d83a6a4d816689a61d1c5f
barendan <barendan@gmail.com>
2018-06-22 08:10:10 -0400

    Backup files

--- src/discover.jsx
+++ src/discover.jsx
@@ -3,9 +3,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Button from './components/Button';
 import Logo from './components/Logo';
 import Suggest from './components/Suggest';
+import Button from './components/Button';
+import Rating from './components/Rating';
 
 
 ReactDOM.render(
@@ -28,6 +29,11 @@ ReactDOM.render(
   	<h2>Suggest</h2>
   	<div><Suggest options={['eenie', 'meenie', 'mminey', 'mo']} /></div>
 
+  	<h2>Rating</h2>
+  	<div>No initial value: <Rating /></div>
+  	<div>Initial value 4: <Rating defaultValue={4} /></div>
+  	<div>This one goes to 11: <Rating defaultValue={11} /></div>
+  	<div>Read-only: <Rating readonly={true} defaultValue={3} /></div>
 
   </div>,
   	document.getElementById('app')
